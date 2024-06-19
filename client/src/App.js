863dd937bd68783c2ddee728b1afd3fd72ef04cb
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-06-19 21:00:19 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/app.scss";
+import Toolbar from "./components/Toolbar";
+import SettingsBar from "./components/SettingsBar";
+import Canvas from "./components/Canvas";
+
+function App() {
+  return <div className="App">
+    <Toolbar />
+    <SettingsBar />
+    <Canvas />
+  </div>;
+}
+
+export default App;
---
+++ client/src/components/SettingsBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/toolbar.scss";
+
+const SettingsBar = () => {
+  return <div className="setting-bar"></div>;
+};
+
+export default SettingsBar;
