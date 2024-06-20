e512dc3891e36b8c47eabf24e5f3c83253c89beb
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-06-20 20:16:32 +0300

    created websocket and server-side event listeners

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
-import "./styles/app.scss";
+import React from 'react';
+import "./styles/app.scss"
+import SettingBar from "./components/SettingBar";
 import Toolbar from "./components/Toolbar";
-import SettingsBar from "./components/SettingsBar";
 import Canvas from "./components/Canvas";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return <div className="App">
-    <Toolbar />
-    <SettingsBar />
-    <Canvas />
-  </div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path='/:id' element={<>
+            <Toolbar/>
+            <SettingBar/>
+            <Canvas/>
+          </>}/>
+          <Route path='*' element={<Navigate to={`f${(+new Date).toString(16)}`} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
 
 export default App;
